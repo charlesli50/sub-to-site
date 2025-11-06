fb79fb1e5fa0d52313c539c5cafd611edae0a9d3
TingYi Wu <tingyi.wu1@gmail.com>
2025-11-05 22:25:03 -0500

    format

--- subscribe.py
+++ subscribe.py
@@ -5,12 +5,12 @@ from difflib import *
 from hashlib import sha256
 from pprint import pprint
 
+
 async def subscribe_and_ping(url: str, script: str) -> None:
-    
     # previous_html_hash = None
     previous_html = None
     ping_idx = 0
-    
+
     async with aiohttp.ClientSession() as session:
         while True:
             async with session.get(url) as response:
@@ -19,33 +19,41 @@ async def subscribe_and_ping(url: str, script: str) -> None:
                 #     f.write(html)
                 ping_idx += 1
                 diff = []
-                
+
                 if previous_html != None:
-                    diff = list(unified_diff(previous_html.splitlines(), new_html.splitlines(), n=0, lineterm=''))
+                    diff = list(
+                        unified_diff(
+                            previous_html.splitlines(),
+                            new_html.splitlines(),
+                            n=0,
+                            lineterm="",
+                        )
+                    )
                     for d in diff:
                         print(d)
                     # if diff:
                     #     pprint(diff)
-                
+
                 if diff and previous_html != None:
                     print("Divergence Encountered! Publishing!")
                     return
-                
+
                 previous_html = new_html
                 await asyncio.sleep(1)
-            
+
 
 async def main(NETWORK_URL: str, TARGET_SCRIPT: str):
     await subscribe_and_ping(NETWORK_URL, TARGET_SCRIPT)
     # time.sleep(1)
 
+
 if __name__ == "__main__":
     args = sys.argv
- 
+
     if len(args) != 3:
         print("Usage: python3 subscribe.py {url} {script}")
-  
+
     NETWORK_URL = args[1]
     TARGET_SCRIPT = args[2]
     asyncio.run(main(NETWORK_URL, TARGET_SCRIPT))
- 
+
